62f06cb76433a53069e38253c47bf617794945f2
Balafama D <121131723+facusfacus@users.noreply.github.com>
2024-12-11 11:14:48 -0500

    Update linear_regression_market.py

--- linear_regression_market.py
+++ linear_regression_market.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
 def load_data(file_path):
